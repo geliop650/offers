f52883ba1be72b4e3078858ef7a5a983ecb7095c
Yorgos Eliopoulos <7126060+geliop650@users.noreply.github.com>
2021-06-07 08:19:12 +0300

    Custom Exception for Request

---
+++ Exceptions.java
@@ -0,0 +1,11 @@
+class NotAvailableRequestException extends Exception{  
+	NotAvailableRequestException(String s){  
+	 super(s);  
+	}  
+ }  
+
+ class InvalidRequestException extends Exception{  
+	InvalidRequestException(String s){  
+	 super(s);  
+	}  
+ }  
--- Requests.java
+++ Requests.java
@@ -1,39 +1,48 @@
-public class Requests extends RequestDonationList
-{
-    public boolean validRequestDonation(Beneficiary b)
-    {
-        for (RequestDonation r : rdEntities){
-            if(!r.isValid(b)){
-                return false;
-            }
-        }
-        return true;
-    }
-    public void add(Organization o1, RequestDonation rd, Beneficiary b){
-        if(o1.isAvailable(rd)){//overriding τα ifs
-            if(validRequestDonation(b)){
-                add(rd);
-            }//else ; exception
-        }//else ; exception
-    }
-    public void modify(Organization o1, RequestDonation rd, Beneficiary b, double q){
-        if(o1.isAvailable(rd)){//overriding τα ifs
-            if(validRequestDonation(b)){
-                modify(rd, q);
-            }//else ; exception
-        }//else ; exception
-    }
-    public void commit(Organization o1, RequestDonation rd, Beneficiary b){
-        if(o1.isAvailable(rd)){//overriding τα ifs
-            if(validRequestDonation(b)){
-                for(RequestDonation cr : rdEntities){
-                    if(cr.getName().equals(rd.getName())){
-                        cr.removeQuantity(rd.getQuantity());
-                        b.addReceived(rd);
-                        return;
-                    }
-                }
-            }//else ; exception
-        }//else ; exception
-    }
+public class Requests extends RequestDonationList {
+	public boolean validRequestDonation(Beneficiary b) {
+		for (RequestDonation r : rdEntities) {
+			if (!r.isValid(b)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void add(Organization o1, RequestDonation rd, Beneficiary b) {
+		try {
+			if (o1.isAvailable(rd)) {// overriding τα ifs
+				if (validRequestDonation(b)) {
+					add(rd);
+				} else {
+					throw new NotAvailableRequestException(String.format("Request: %s", rd.getName()));
+				}
+			} else {
+				throw new InvalidRequestException(String.format("Request: %s", rd.getName()));
+			}
+		} catch (Exception m) {
+			System.out.println(String.format("Exception occured: %s",m));
+		}
+	}
+
+	public void modify(Organization o1, RequestDonation rd, Beneficiary b, double q) {
+		if (o1.isAvailable(rd)) {// overriding τα ifs
+			if (validRequestDonation(b)) {
+				modify(rd, q);
+			} // else ; exception
+		} // else ; exception
+	}
+
+	public void commit(Organization o1, RequestDonation rd, Beneficiary b) {
+		if (o1.isAvailable(rd)) {// overriding τα ifs
+			if (validRequestDonation(b)) {
+				for (RequestDonation cr : rdEntities) {
+					if (cr.getName().equals(rd.getName())) {
+						cr.removeQuantity(rd.getQuantity());
+						b.addReceived(rd);
+						return;
+					}
+				}
+			} // else ; exception
+		} // else ; exception
+	}
 }
